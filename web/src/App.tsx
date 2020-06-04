fe3db2ad7786e8ed1fe73eb6dc6401909fc62917
Matheus Kolln <matheuzhenrik@gmail.com>
2020-06-03 23:00:22 -0300

    Dia 3

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Back-end > Front-end
-        </p>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
